ab5008bbd92dfb825133114c23b7681f1f75b6b2
Satyam Singh <satyams2@illinois.edu>
2024-03-13 21:02:29 -0500

    experience scroll bar layout set

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import MainLayout from "@/components/MainLayout";
+import Experience from "@/components/Experience";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +14,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <MainLayout>{children}</MainLayout></body>
+        <MainLayout>{children}</MainLayout>
+        </body>
+        <body>
+          <Experience></Experience>
+        </body>
+        
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,26 +1,23 @@
 import React from "react";
-import image from "../images/profile.jpg";
 const Home = () => {
   return (
-    <div className="pt-10 gap-4 ">
-      {/* <img
-        src="https://media.licdn.com/dms/image/C4D03AQEh5TQYOVabFg/profile-displayphoto-shrink_400_400/0/1658277944683?e=1715817600&v=beta&t=6wJXkNsW_zn2-UwrWLKllZBJK_oglZziLGyoAbSV_Sc"
-        class="rounded-full h-auto max-w-sm mx-auto border border-4 border-white"
-        alt="Self Picture"
-      /> */}
+    <div className="pt-10 gap-4 flex flex-row justify-center">
+      <div className="px-10">
+        <img
+          src="https://media.licdn.com/dms/image/C4D03AQEh5TQYOVabFg/profile-displayphoto-shrink_400_400/0/1658277944683?e=1715817600&v=beta&t=6wJXkNsW_zn2-UwrWLKllZBJK_oglZziLGyoAbSV_Sc"
+          class="rounded-full h-auto max-w-sm mx-auto border border-4 border-white"
+          alt="Self Picture"
+        />
+      </div>
 
-      <h1 className="mt-10 text-center text-6xl font-sans-bold leading-9 tracking-tight text-white">
-        Satyam Singh
-      </h1>
-      <div className="flex items-center justify-center mt-10">
-        <a href="https://grainger.illinois.edu/academics/undergraduate/majors-and-minors/computer-engineering">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Illinois_Block_I.png"
-            class="h-auto h-10 w-10 mr-2"
-            alt="Self Picture"
-          />
-        </a>
-        <p className="text-3xl text-center"> Computer Engineering @ UIUC</p>
+      <div className="pt-5">
+        <h1 className="mt-10 text-center text-6xl font-sans-bold leading-9 tracking-tight text-white">
+          Satyam Singh
+        </h1>
+        <div className="flex flex-col items-center justify-center mt-10">
+          <p className="text-3xl text-center"> Computer Engineering @ UIUC</p>
+          <p className="text-3xl text-center py-5"> Innovation, Leadership & Engineering Entrepreneurship @ UIUC</p>
+        </div>
       </div>
     </div>
   );
