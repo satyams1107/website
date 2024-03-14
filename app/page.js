3a2d06917d58c1fe6aed35393962469344aa9a3e
Satyam Singh <satyams2@illinois.edu>
2024-03-14 01:56:53 -0500

    experience cards and UI glow up

--- app/page.js
+++ app/page.js
@@ -1,8 +1,9 @@
 import React from "react";
 const Home = () => {
   return (
-    <div className="pt-10 gap-4 flex flex-row justify-center">
-      <div className="px-10">
+    <div className="py-10 gap-4 flex flex-row justify-center h-[550px] w-full h-full bg-no-repeat bg-cover
+    bg-[url('https://cdn.pixabay.com/photo/2024/01/30/14/02/road-8542123_1280.jpg')]"  >
+      <div className="px-10 pt-10 drop-shadow-lg">
         <img
           src="https://media.licdn.com/dms/image/C4D03AQEh5TQYOVabFg/profile-displayphoto-shrink_400_400/0/1658277944683?e=1715817600&v=beta&t=6wJXkNsW_zn2-UwrWLKllZBJK_oglZziLGyoAbSV_Sc"
           class="rounded-full h-auto max-w-sm mx-auto border border-4 border-white"
@@ -10,13 +11,23 @@ const Home = () => {
         />
       </div>
 
-      <div className="pt-5">
+      <div className="drop-shadow-lg">
         <h1 className="mt-10 text-center text-6xl font-sans-bold leading-9 tracking-tight text-white">
           Satyam Singh
         </h1>
         <div className="flex flex-col items-center justify-center mt-10">
-          <p className="text-3xl text-center"> Computer Engineering @ UIUC</p>
-          <p className="text-3xl text-center py-5"> Innovation, Leadership & Engineering Entrepreneurship @ UIUC</p>
+          <p className="text-3xl text-center"> Computer Engineering</p>
+          <p className="text-3xl text-center py-5">
+            {" "}
+            Innovation, Leadership & Engineering Entrepreneurship
+          </p>
+          <a href="https://grainger.illinois.edu/academics/undergraduate/majors-and-minors/computer-engineering">
+          <img
+            className="w-[100px] pt-5"
+            src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Illinois_Block_I.png"
+            alt="UIUC"
+          />
+          </a>
         </div>
       </div>
     </div>
